7c5e7300403555e52ef3ff5892d57510b37a02f6
JoonHyug <hyug0819@gmail.com>
2022-04-13 18:40:07 +0900

    테스트 성공1

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var bodyParaser = require('body-parser');
 
-app.listen(3000, function(){
-    console.log("start! express server on port 3000");
-    console.log("111aaaaaaaasd123sad");
-})
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParaser.urlencoded({extended: true}));
+
+app.get('/process/login', function(req, res) {	
+	var paramId = req.param('id');
+	
+	console.log('/precess/login 처리, id: '+ paramId);
+
+	res.write("Success");
+	res.end();
+});
+
+http.createServer(app).listen(3000, function() {
+	console.log('Express 서버가 3000번 포트에서 시작됨.');
+});
